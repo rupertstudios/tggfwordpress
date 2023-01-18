a5f7c59a74db5af642acf67d875049ae5c9b67c0
rupertstudios <93224822+rupertstudios@users.noreply.github.com>
2023-01-18 20:59:56 +0100

    Update cooking.php

--- wp-content/mu-plugins/cooking.php
+++ wp-content/mu-plugins/cooking.php
@@ -1,3 +1,4 @@
+<?php
 /**
  * Force consent cookies on the root as shown below, or choose a subfolder
  * Add subfolder between '' on line 9 e.g. /subfolder/
@@ -8,3 +9,4 @@ function my_cookie_path($path) {
     return '';
 }
 add_filter( 'cmplz_cookie_path', 'my_cookie_path');
+?>
