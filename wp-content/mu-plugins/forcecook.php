571a62ceceebd4e92545ecf1a1e6732a09031171
rupertstudios <93224822+rupertstudios@users.noreply.github.com>
2023-01-18 19:15:47 +0100

    Delete wp-content/mu-plugins directory

--- wp-content/mu-plugins/forcecook.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* Force consent cookies on the root as shown below, or choose a subfolder 
-* Add subfolder between '' on line 6 e.g. /subfolder/
-*/
-function my_cookie_path($path) {
-    return '';
-}
-add_filter( 'cmplz_cookie_path', 'my_cookie_path'); 
