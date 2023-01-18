5109aeb770430fc1a5a25829276ac48fcc3edf8f
rupertstudios <93224822+rupertstudios@users.noreply.github.com>
2023-01-18 20:09:09 +0100

    Create cooking.php

---
+++ wp-content/mu-plugins/cooking.php
@@ -0,0 +1,10 @@
+/**
+ * Force consent cookies on the root as shown below, or choose a subfolder
+ * Add subfolder between '' on line 9 e.g. /subfolder/
+ * @param string @path
+ * @return string
+*/
+function my_cookie_path($path) {
+    return '/tggfwordpress/';
+}
+add_filter( 'cmplz_cookie_path', 'my_cookie_path');
